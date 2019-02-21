d6ab9d0ef0d04e83d68c7a67923211f807ccd0c4
DannaCb5 <dannal@bellsouth.net>
2019-02-21 16:16:00 -0500

    21FEB19 041519

--- PHPAssignmets/ToDoDatabase.php
+++ PHPAssignmets/ToDoDatabase.php
@@ -7,37 +7,50 @@ Task: <input type="text" name="taskname">
 </form>
 
 <?php
-
-if(!empty($_POST) && isset($_POST["taskname"])) { 
-    echo "Please enter a task and click the add button.";
-   } else {
-    $taskname_input = $_POST["taskname"];
-    $filtered_task = (filter_var($taskname_input, FILTER_SANITIZE_STRING));
-   }
-}
 $servername = "localhost";
 $username = "root";
 $password = "root";
 $dbname = "to_do_list";
 
+
 try {
     $conn = new PDO("mysql:host=$servername; dbname=$dbname", $username, $password);
 
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "INSERT INTO todo (task) VALUE('$taskname_input')";
-    $conn->exec($sql);
+    
+    if(!empty($_POST) && isset($_POST["taskname"])) { 
+        $taskname_input = $_POST["taskname"];
+        $filtered_task = (filter_var($taskname_input, FILTER_SANITIZE_STRING));
+        $sql = "INSERT INTO todo (task) VALUE('$taskname_input')";
+        $conn->exec($sql);
+    } else {
 
-    $task = $conn->prepare("SELECT task FROM todo"); 
+    echo "Please enter a task and click the add button.";
+    }
+
+    $task = $conn->prepare("SELECT * FROM todo"); 
     $task->execute();
 
     // set the resulting array to associative
     $task->setFetchMode(PDO::FETCH_ASSOC);
     $results = $task->fetchAll();
+
     foreach ($results as $key => $value) {
 ?>
+      
       <li> <?php echo $value["task"]; ?></li> 
+           <form action="ToDoDatabase.php" method="post">
+               
+               <input type="text" value= "<?php echo $value["id"]; ?>" name="id" hidden>
+               <input type= "submit" value= "Done">
+
+           </form>
+            
 <?php
     }
+        $id = $_POST['id'];
+        $sql = "DELETE FROM todo WHERE id = $id";
+        $conn->exec($sql);
 
 } catch (PDOException $e) {
     echo $sql . "<br>" . $e->getMessage();
