780cff971a2d14be805ce44d74b976c17fade8d1
DannaCb5 <dannal@bellsouth.net>
2019-03-22 15:46:02 -0400

    032219 0345pm

--- PHPAssignmets/EventAttendanceTracker/Admin.php
+++ PHPAssignmets/EventAttendanceTracker/Admin.php
@@ -15,35 +15,37 @@ session_start();
 
 // Set session variables
 if(isset($_POST['submit'])) {
-$servername = "localhost";
-$user = "root";
-$password = "root";
-$dbUName = "uname";
-$dbPWord = "pword";
-$dbname = "admin_authentication";
-$tbname = "authenticate_username_and_password";
-$formUName = $_POST["formUName"];
-$formPWord = $_POST["formPWord"];
-$_SESSION["formUName"] = $formUName;
-$_SESSION["formPWord"] = $formPWord;
-$conn = new PDO("mysql:host=$servername;dbname=$dbname",$user,$password);
-$stmt = $conn->prepare("SELECT * FROM $tbname WHERE uname = '$formUName'");
-$stmt->bindParam(':dbUName', $dbUName);
-$stmt->bindParam(':dbPWord', $dbPWord);
-$stmt->execute();
-$result = $stmt->fetch(PDO::FETCH_ASSOC);
-echo "start"; echo $result; echo "end";
-echo $formUName; echo "<br>";
-echo $formPWord; echo "<br>";
-echo $dbUName; echo "<br>";
-echo $dbPWord; echo "<br>"; 
+    $servername = "localhost";
+    $user = "root";
+    $password = "root";
+    $dbUName = "uname";
+    $dbPWord = "pword";
+    $dbname = "admin";
+    $tbname = "authenticate";
+    $formUName = $_POST["formUName"];
+    $formPWord = $_POST["formPWord"];
+    $_SESSION["formUName"] = $formUName;
+    $_SESSION["formPWord"] = $formPWord;
+    $conn = new PDO("mysql:host=$servername;dbname=admin",$user, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // $stmt = $conn->prepare("SELECT ?, ? FROM authenticate");
+    // $stmt->execute();
+    // $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+    // // $stmt->bindParam(':dbUName', uname);
+    // // $stmt->bindParam(':dbPWord', $dbPWord);
+    // // $stmt->execute();
+    // $results = $stmt->fetchAll();
 
-echo "Session variables are set."; echo "<br>";
-if ($dbUName == $formUName && $dbPWord == $formPWord) {
-    echo "User Name and password have been authenticated"; echo "<br>";
-} else { 
-    echo "Your User Name and/or Password are not correct"; echo "<br>";
-}
+    $stmt = $conn->prepare("SELECT 1 FROM authenticate WHERE uname=? and pword=?");
+    $stmt->execute([$formUName, $formPWord]);
+    $userExists = $stmt->fetchColumn();
+
+    echo "Session variables are set."; echo "<br>";
+    if ($userExists) {
+        echo "User Name and password have been authenticated"; echo "<br>";
+    } else { 
+        echo "Your User Name and/or Password are not correct";echo "<br>";
+    }
 } else {
     echo "You didn't enter anything."; echo "<br>";
 // Destroying the session clears the $_SESSION variable, thus "logging" the user
